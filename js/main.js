d34fe0b2e152ed17f1ebce32118afa99c7b7e745
Hasan Ahmed Noyon <31069132+HasanAhmedNoyon@users.noreply.github.com>
2021-07-13 16:26:40 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 2,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            768:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:2,
+                nav:true,
+                loop:false
+            }
+        }
+    });
+  }); 
